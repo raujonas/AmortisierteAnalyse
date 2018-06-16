32be036aad94999e0add0a4e47e7faa40f27c0f5
DESKTOP-GPL2RIM\Jonas <rau.jonas@web.de>
2018-06-16 14:27:20 +0200

    nice

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,22 @@ public class Main {
     public static void main(String[] args) {
 
         int i = 0;
-        int n = 3;
+        int n = 5;
         int A[] = new int[n];
+        int konto = 0;
 
         do {
+            konto += 2;
             i = 1;
 
             while (A[i-1]==1){
                 A[i-1] = 0;
+                konto -=1;
                 i+=1;
             }
 
             A[i-1] = 1;
+            konto -=1;
 
         } while (i<n);
     }
